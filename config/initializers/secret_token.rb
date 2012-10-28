da6783588da408643960d8ae1717d551c2be7b0e
Bryce Kerley <bkerley@brycekerley.net>
2012-10-28 00:33:21 -0400

    xss playground initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XssPlayground::Application.config.secret_token = 'fb8e88a553e4bc641decc5926d4e4d37c7295038b579f1612cb612c822a9ba92f12d5f65ef174315cccdea59e95d9e203b9535007f1d752e33d1f6ce26571370'
